299f6cea346cff7e83fc0747181356107611e599
Rafael Gonzalez <astrorafael@yahoo.es>
2021-09-21 12:32:29 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+# ACTION deployment tool
+
+This tool helps deployment of several Airflow components for ACTION Pilots
+Each pilot has its how deployment needs
+
+* [Street Spectra Deployment](StreetSpectra.md)
---
+++ setup.py
@@ -0,0 +1,67 @@
+import os
+import os.path
+
+from setuptools import setup, find_packages, Extension
+import versioneer
+
+# Default description in markdown
+LONG_DESCRIPTION = open('README.md').read()
+
+
+PKG_NAME     = 'action-tool'
+AUTHOR       = 'Rafael Gonzalez'
+AUTHOR_EMAIL = 'astrorafael@gmail.com'
+DESCRIPTION  = 'Deployment tool for Airflow ACTION project pilots',
+LICENSE      = 'MIT'
+KEYWORDS     = 'Astronomy Python CitizenScience LightPollution'
+URL          = 'https://github.com/actionprojecteu/action-tool/'
+DEPENDENCIES = []
+
+CLASSIFIERS  = [
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering :: Astronomy',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+    'Development Status :: 4 - Beta',
+]
+
+PACKAGE_DATA = {
+    'actiontool': [
+        'data/dags/*.py',
+        'data/sql/*.sql'
+    ],
+}
+
+SCRIPTS = [
+    "scripts/actiontool"
+]
+
+
+DATA_FILES  = [ 
+
+]
+
+setup(
+    name             = PKG_NAME,
+    version          = versioneer.get_version(),
+    cmdclass         = versioneer.get_cmdclass(),
+    author           = AUTHOR,
+    author_email     = AUTHOR_EMAIL,
+    description      = DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    long_description = LONG_DESCRIPTION,
+    license          = LICENSE,
+    keywords         = KEYWORDS,
+    url              = URL,
+    classifiers      = CLASSIFIERS,
+    packages         = find_packages("src"),
+    package_dir      = {"": "src"},
+    install_requires = DEPENDENCIES,
+    scripts          = SCRIPTS,
+    package_data     = PACKAGE_DATA,
+    data_files       = DATA_FILES,
+)
